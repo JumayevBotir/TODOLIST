fc17ecba4cc0a0f93c7e5f3c1f18b30dfd39c753
JumayevBotir <jumayevbotirbek66@gmail.com>
2024-09-23 10:05:55 -0700

    wkjefhweffe

---
+++ index.js
@@ -0,0 +1,49 @@
+let result=document.getElementById('number');
+let result1=document.getElementById('btn');
+let form=document.getElementById('form')
+let a=['noll','bir','ikki','uch','tort','besh','olti','yetti','sakkiz',`to'qqiz`,`o'n`];
+let b=["","",'yegirma',`o'ttiz`,'qirq','ellik','oltmish','yetmish','sakson',`to'qson`];
+let c='yuz';
+function son(n){
+    if(n<0||n>999){
+        return(`son 0<n<999 oraliqda bolishi kerak`)
+    }
+    else if(n<=10) {
+        return(a[n])
+    }
+    else if(n<=99){
+        let r=n%10;
+        let q=Math.floor(n/10);
+        if(q==1){
+            return(a[10]+" "+a[r]);
+        }
+        else{
+            return(b[q]+" "+a[r]);
+        }
+    }
+    else if(n<=999) {
+        let res=n%100;
+        let res4=Math.floor(res/10);
+        let res1=Math.floor(n/100);
+        let res3=n%10;
+        if(res3==0){
+            return(a[res1]+' '+c+' '+b[res4])
+        }
+        else{
+            return(a[res1]+' '+c+' '+b[res4]+' '+a[res3])
+        }
+    }    
+}son(1000);
+
+result1.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let n=parseInt(result.value)
+    let otput=son(n);
+   form.innerHTML=otput;
+})
+
+
+
+
+
+
